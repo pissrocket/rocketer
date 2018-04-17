b3ce6f6b993befc7902004a9cce3912fc585328c
Rettend <36823200+Rettend@users.noreply.github.com>
2018-04-17 21:35:18 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -285,11 +285,13 @@ async def on_message(message):
         await bot.send_message(message.channel, embed=em)  
     if message.content.startswith('r-bot'):
         await bot.send_message(message.channel, "```md\n"
-                                "<\\______\\______//THE-ROCKETER-BO\\______//______//>\n"
+                                "<⊐______⊐______⊏THE-ROCKETER-BOT⊐______⊏______⊏>\n"
                                 "\n"
+                                "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
                                 "\n"
-                                "<˙˙˙˙˙˙˙˙˙˙˙The-Official-Bot-of-PissRocket.˙˙˙˙˙˙˙˙˙˙˙>\n"
-                                "<˙˙˙˙˙˙˙˙˙˙˙The-currently-version-is-{}-!˙˙˙˙˙˙˙˙˙˙˙˙>\n"
+                                "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n"
+                                "<˙˙˙˙˙˙˙˙˙The-Official-Bot-of-PissRocket.˙˙˙˙˙˙˙˙˙>\n"
+                                "<˙˙˙˙˙˙˙˙The-currently-version-is-{}-!˙˙˙˙˙˙˙˙˙˙˙˙˙˙>\n"
                                 "           for the commands, type: \"r-help\"```".format(version))
 bot.process_commands(message)
 
