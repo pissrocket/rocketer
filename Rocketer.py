2f83ffccaf7a5635cc84681f204657cf645dc4d0
Rettend <36823200+Rettend@users.noreply.github.com>
2018-04-28 11:11:26 +0200

    Add files via upload

--- Rocketer.py
+++ Rocketer.py
@@ -9,12 +9,6 @@ import datetime
 import traceback
 import os
 import sys
-from ext import embedtobox
-import psutil
-import random
-import pip
-import json
-import io
 
 version = "0.4.4"
 owner = ["361534796830081024"]
@@ -321,48 +315,6 @@ async def on_message(message):
                                 "         for the commands, type: \"r-help\"```".format(version), colour=0x3498db)
         await bot.send_message(message.channel, embed=em)
 bot.process_commands(message)
-
-class Information:
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command(no_pm=True)
-    async def channels(self, ctx, serverid:int = None):
-
-        if serverid is None:
-            server = ctx.guild
-        else:
-            server = discord.utils.get(self.bot.guilds, id=serverid)
-            if server is None:
-                return await ctx.send('Server not found!')
-
-        e = discord.Embed()
-        e.color = await ctx.get_dominant_color()
-
-        voice = ''
-        text = ''
-        categories = ''
-
-        for channel in server.voice_channels:
-            voice += f'\U0001f508 {channel}\n'
-        for channel in server.categories:
-            categories += f'\U0001f4da {channel}\n'
-        for channel in server.text_channels:
-            text += f'\U0001f4dd {channel}\n'
-        
-        if len(server.text_channels) > 0:
-            e.add_field(name='Text Channels', value=f'```{text}```')
-        if len(server.categories) > 0:
-            e.add_field(name='Categories', value=f'```{categories}```')
-        if len(server.voice_channels) > 0:
-            e.add_field(name='Voice Channels', value=f'```{voice}```')
-
-        try:
-            await ctx.send(embed=e)
-        except discord.HTTPException:
-            em_list = await embedtobox.etb(e)
-            for page in em_list:
-                await ctx.send(page)
 
 
 
