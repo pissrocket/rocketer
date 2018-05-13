2a72493bff7dc710f9409bd3a3b8926dcc557ecb
Rettend <36823200+Rettend@users.noreply.github.com>
2018-05-13 15:36:54 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -45,7 +45,7 @@ async def suggest(ctx, pref, text):
     finally:
         colours = [0x11806a, 0x1abc9c, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xe67e22, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a]
         col = random.choice(colours)
-        em = discord.Embed(title=f"{msg}", description=f"**From {member.nick}**\n⋙ {text}", colour=col)
+        em = discord.Embed(title=f"{msg}", description=f"**From {ctx.message.author.mention}**\n⋙ {text}", colour=col)
         channel = bot.get_channel(id="444837114258128916")
         room = bot.get_channel(id="444837114258128916")
         await bot.send_message(message.channel, f"**:white_check_mark: Sent in {channel}**")
