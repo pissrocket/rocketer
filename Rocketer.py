7ed210ffef1c50124af0898110f2ff670b98a046
Rettend <36823200+Rettend@users.noreply.github.com>
2018-05-18 22:33:44 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -115,18 +115,17 @@ async def poll(ctx, question, options: str):
     if len(options) > 10:
         await bot.say('You cannot make a poll for more than 10 things!')
         return
-    if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+    if len(options) == 2:
         reactions = ['✅', '❌']
     else:
         reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
     description = []
     for x, option in enumerate(options):
         description += '\n {} {}'.format(reactions[x], option)
-    embed = discord.Embed(title=question, description=''.join(description))
+    embed = discord.Embed(title=question, description=''.join(description), colour=0x3498db)
     react_message = await bot.say(embed=embed)
     for reaction in reactions[:len(options)]:
         await bot.add_reaction(react_message, reaction)
-    embed.set_footer(text='Poll ID: {}'.format(react_message.id))
     await bot.edit_message(react_message, embed=embed)
     
 @bot.event
