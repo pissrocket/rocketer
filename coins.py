1f2f140a76aa8c56c0564a48094fde34ba8191ba
Rettend <36823200+Rettend@users.noreply.github.com>
2018-05-25 17:32:32 +0200

    Update coins.py

--- coins.py
+++ coins.py
@@ -18,12 +18,12 @@ class Coin:
         self.COIN_INTERVAL = 30                          # interval in seconds for sending out coins.
 
     @commands.command(name="coins", pass_context=True, help="Get your coin amount")
-    async def get_coins(self, ctx):
+    async def coins(self, ctx):
         await self.bot.say("{}, you have {} coins".format(ctx.message.author.mention,
                                                           self.database.get_coins(ctx.message.author.id)))
 
-    @commands.command(name="roll", pass_context=True, help="Gamble coins, reach over 50 - 100")
-    async def roll_dice(self, ctx, amount):
+    """@commands.command(name="roll", pass_context=True, help="Gamble coins, reach over 50 - 100")
+    async def luck(self, ctx, amount):
         if float(amount) <= 0 :
             await self.bot.say("{}, please enter a valid amount?".format(ctx.message.author.mention))
             return None
@@ -44,10 +44,10 @@ class Coin:
             self.database.insert_coins(ctx.message.author.id, amount, ctx.message.author.mention)
             await self.bot.say(
                 "{}, you won! you rolled {} and won {} coins".format(ctx.message.author.mention, rolled, amount))
-            pass
+            pass"""
 
     @commands.command(name="donate", pass_context=True)
-    async def donate_coins(self, ctx, toUser :discord.Member, coins):
+    async def give(self, ctx, toUser :discord.Member, coins):
         member = toUser
         if member is not None :
             if self.check_balance(ctx.message.author, float(coins)):
@@ -63,7 +63,7 @@ class Coin:
         else: await self.bot.say("Did not find member {}".format(toUser))
 
     @commands.command(name="toplist", pass_context=False)
-    async def get_toplist(self):
+    async def toplist(self):
         output = "On the coin top we got: \n \n"
         count = 1
         for user in self.database.get_top_coin_holders() :
@@ -87,45 +87,7 @@ class Coin:
             return False
         return True
 
-    async def give_coin(self):
-        '''
-        This is used to give coins every interval set by object vars...
-        The bot will take a tax fee for being in a voice channel if taxAble is true
-        '''
-        while self.coinActive:
-            members = self.get_all_voice_members_except_in_afk()
-            total_tax = self.COIN_AMOUNT * self.tax.tax_amount_percentage
-            done_taxed_coins = self.COIN_AMOUNT - total_tax
-            for m in members:
-                # The bot collects the total_tax for all members.
-                if self.tax.taxable and m.id == self.bot.user.id:
-                        self.database.insert_coins(m.id, total_tax, m.mention)
-                # if this user is the bot, continue to next iteration.
-                if m.id == self.bot.user.id:
-                    continue
-
-                self.database.insert_coins(m.id, done_taxed_coins, m.mention)
-            await asyncio.sleep(self.COIN_INTERVAL)
-
-    def get_all_voice_members_except_in_afk(self):
-        '''
-        get every member of voice channels except channels named AFK.
-        :return:    a list of members
-        '''
-        members = []
-        for server in self.bot.servers:
-            for channel in server.channels :
-
-                if channel.name != "AFK":
-                    # If this returns 0, it means that either the room is empty or it isn't a voice channel
-                    if len(channel.voice_members) != 0:
-                        voice_members = channel.voice_members
-                        for member in voice_members:
-                            members.append(member)
-
-        return members
-
-
+    
 def setup(bot):
     bot.add_cog(Coin(bot))
 
