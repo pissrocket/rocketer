8f94e99bcdf59cc6103db77248cd021e89b0d03d
Rettend <36823200+Rettend@users.noreply.github.com>
2018-05-26 21:19:25 +0200

    Update Rocketer.py

--- Rocketer.py
+++ Rocketer.py
@@ -41,7 +41,7 @@ async def purge(ctx, number : int):
     if "-----BIG ROCKET------" or "----HEAD ADMIN-----" or "----------MOD----------" or "-------HEAD MOD-----" or "---------ADMIN--------" or "----BABY ROCKET----" in [y.name.lower() for y in ctx.message.author.roles]:
         number += 1
         deleted = await bot.purge_from(ctx.message.channel, limit=number)
-        await bot.send_message(ctx.message.channel, f'**{ctx.message.author} deleted {deleted} messages**')
+        await bot.send_message(ctx.message.channel, f'**{ctx.message.author} deleted __{number}__ messages**')
     else:
         raise NoPermError
         
